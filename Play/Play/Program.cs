7267eac70d3b64707aaa43a47757f501583b47de
oleg1625 <olegdobrylev95@gmail.com>
2025-11-05 21:11:57 +0300

    1

--- Play/Play/Program.cs
+++ Play/Play/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Play.Tanks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,10 +15,10 @@ namespace Play
 
             bool trigger = true;
 
-            while (trigger) 
+            while (trigger)
             {
-               int tank_selection = Convert.ToInt32(Console.ReadLine());
-               switch (tank_selection) 
+                int tank_selection = Convert.ToInt32(Console.ReadLine());
+                switch (tank_selection)
                 {
                     case 1:
                         Console.WriteLine("Вы выбрали Panther D");
@@ -32,29 +33,110 @@ namespace Play
                         trigger = false;
                         break;
                     default:
-                        Console.Write("Нкорректный ввод. Выберите ещё раз: ");
+                        Console.Write("Некорректный ввод. Выберите ещё раз: ");
                         break;
 
-                } 
-            } 
+                }
+            }
 
             Console.ReadKey();
-            
+
         }
     }
 
-    namespace Tanks 
+    namespace Tanks
     {
         public abstract class Tank
         {
             public abstract int HP { get; protected set; }
+            public abstract int Damage { get; protected set; }
+
+            public void Shot(Tank a, int damage) 
+            {
+                a.HP -= damage;
+            }
 
         }
-        }
+    
 
-        public class T34 : Tank
+        public class T34_85 : Tank
         {
-            public
+            private int _hp = 100;
+            private int _damage = 15;
+            public override int HP 
+            { 
+                get => _hp; 
+                protected set 
+                {
+                    _hp = value;        
+                } 
+            }
+
+            public override int Damage 
+            {
+                get => _damage;
+                protected set 
+                {
+                    _damage = value;
+                }
+            }
+
+        }
+        public class Panther_D : Tank
+        {
+            private int _hp = 120;
+            private int _damage = 25;
+            public override int HP
+            {
+                get => _hp;
+                protected set
+                {
+                    _hp = value;
+                }
+            }
+
+            public override int Damage
+            {
+                get => _damage;
+                protected set
+                {
+                    _damage = value;
+                }
+            }
+
+        }
+        public class M4_Sherman : Tank
+        {
+            private int _hp = 140;
+            private int _damage = 10;
+            public override int HP
+            {
+                get => _hp;
+                protected set
+                {
+                    _hp = value;
+                }
+            }
+
+            public override int Damage
+            {
+                get => _damage;
+                protected set
+                {
+                    _damage = value;
+                }
+            }
+
+        }
+
+    }
+
+    namespace system_void 
+    {
+        public static void Registration() 
+        {
+        
         }
     }
+    
 }
